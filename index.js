f0125d7df842cebe838905c20b7360ae4bce605b
Zac Willmington <zac.willmington@gmail.com>
2018-09-08 00:14:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value){
-    const newDriver = Object.assign({}, driver, key, value);
+    const newDriver = {...driver};
+    newDriver[key] = value;
     return newDriver;
 }
